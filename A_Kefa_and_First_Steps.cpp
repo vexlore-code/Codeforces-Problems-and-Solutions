b281158a5b312e259787df3b6983f9db00c434e3
Jubayer Hasan Munna <185611999+vexlore-code@users.noreply.github.com>
2025-07-04 21:43:17 +0600

    Initial commit

--- A_Kefa_and_First_Steps.cpp
+++ A_Kefa_and_First_Steps.cpp
@@ -1,27 +1,29 @@
 /*
- ╔══════════════════════════════════════╗
-             ✦ v e x l o r e ✦         	   	Created     : 04-07-2025  21:06:10
-   "Code like a poet. Debug like a pro." 	GitHub      : https://github.com/vexlore-code
- ╚══════════════════════════════════════╝
+ "La Ilaha Illallah Muhammadur Rasulullah"
+ ╔═══════════════════════════════════════╗
+             ✦ v e x l o r e ✦         	   		Created	 : 04-07-2025  21:06:10
+   "Code like a poet. Debug like a pro." 		 GitHub  : https://github.com/vexlore-code
+ ╚═══════════════════════════════════════╝
 */
-
+ 
 #include <bits/stdc++.h>
 using namespace std;
-
+ 
 #define fastIO                   \
     ios::sync_with_stdio(false); \
     cin.tie(nullptr);            \
     cout.tie(nullptr);
 #define int long long
 #define endl '\n'
-
+ 
 const int MOD = 1e9 + 7, INF = 1e18, N = 2e5 + 5;
-
+ 
 typedef vector<int> vi;
 typedef pair<int, int> pii;
 typedef vector<pii> vpi;
-
-/* ~~~ MAIN CODE ~~~ */
+/*
+ ═══════════════════════════════════ MAIN CODE ═══════════════════════════════════
+*/
 signed main()
 {
     fastIO;
@@ -39,6 +41,6 @@ signed main()
             max = k;
     }
     cout << max << endl;
-
+ 
     return 0;
 }
