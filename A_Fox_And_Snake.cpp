a3aa39415007ff6c9d97e5940bf6fc1448a6bb37
vexlore-code <jubiofficial.dev@gmail.com>
2025-07-08 22:22:10 +0600

    Initial commit

---
+++ A_Fox_And_Snake.cpp
@@ -0,0 +1,54 @@
+/*
+ "La Ilaha Illallah Muhammadur Rasulullah"
+ ╔═══════════════════════════════════════╗
+             ✦ v e x l o r e ✦         	   		Created	 : 08-07-2025  22:20:51
+   "Code like a poet. Debug like a pro." 		 GitHub  : https://github.com/vexlore-code
+ ╚═══════════════════════════════════════╝
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastIO                   \
+    ios::sync_with_stdio(false); \
+    cin.tie(nullptr);            \
+    cout.tie(nullptr);
+#define int long long
+#define endl '\n'
+
+const int MOD = 1e9 + 7, INF = 1e18, N = 2e5 + 5;
+
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+typedef vector<pii> vpi;
+/*
+ ═══════════════════════════════════ MAIN CODE ═══════════════════════════════════
+*/
+signed main()
+{
+    fastIO;
+    int m, n;
+    cin >> m >> n;
+    for (int i = 0; i < m; i++)
+    {
+        if (i % 2 == 0)
+        {
+            for (int j = 0; j < n; j++)
+                cout << "#";
+        }
+        else if (i % 4 == 1)
+        {
+            for (int j = 0; j < n - 1; j++)
+                cout << ".";
+            cout << "#";
+        }
+        else
+        {
+            cout << "#";
+            for (int j = 1; j < n; j++)
+                cout << ".";
+        }
+        cout << '\n';
+    }
+    return 0;
+}
