0d5f60b16167ff7a42afb5fb8c871daaaa45b023
vexlore-code <jubiofficial.dev@gmail.com>
2025-07-15 16:04:33 +0600

    Initial commit

---
+++ B_Equal_Candies.cpp
@@ -0,0 +1,45 @@
+/*
+ "La Ilaha Illallah Muhammadur Rasulullah"
+ ╔═══════════════════════════════════════╗
+             ✦ v e x l o r e ✦         	   		Created	 : 15-07-2025  16:00:49
+   "Code like a poet. Debug like a pro." 		 GitHub  : https://github.com/vexlore-code
+ ╚═══════════════════════════════════════╝
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastIO                   \
+    ios::sync_with_stdio(false); \
+    cin.tie(nullptr);            \
+    cout.tie(nullptr);
+#define int long long
+#define endl '\n'
+
+const int MOD = 1e9 + 7, INF = 1e18, N = 2e5 + 5;
+
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+typedef vector<pii> vpi;
+/*
+ ═══════════════════════════════════ MAIN CODE ═══════════════════════════════════
+*/
+signed main()
+{
+    fastIO;
+    int count, mn;
+    cin >> count;
+    int arr[count];
+    for (int i = 0; i < count; i++)
+    {
+        cin >> arr[i];
+        mn = min(arr[i], mn);
+    }
+    for (int j = 0; j < count; j++)
+    {
+        
+    }
+    
+
+    return 0;
+}
