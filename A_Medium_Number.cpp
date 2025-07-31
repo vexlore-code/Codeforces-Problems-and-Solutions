cb69d4c507ab55cd57da9af22bee08dab7fcdee5
vexlore-code <jubiofficial.dev@gmail.com>
2025-07-31 21:35:26 +0600

    Initial commit

---
+++ A_Medium_Number.cpp
@@ -0,0 +1,41 @@
+/*
+ "La Ilaha Illallah Muhammadur Rasulullah"
+ ╔═══════════════════════════════════════╗
+             ✦ v e x l o r e ✦         	   		Created	 : 31-07-2025  21:30:52
+   "Code like a poet. Debug like a pro." 		 GitHub  : https://github.com/vexlore-code
+ ╚═══════════════════════════════════════╝
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastIO                   \
+    ios::sync_with_stdio(false); \
+    cin.tie(nullptr);            \
+    cout.tie(nullptr);
+#define int long long
+#define endl '\n'
+
+const int MOD = 1e9 + 7, INF = 1e18, N = 2e5 + 5;
+
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+typedef vector<pii> vpi;
+/*
+ ═══════════════════════════════════ MAIN CODE ═══════════════════════════════════
+*/
+signed main()
+{
+    fastIO;
+    int test;
+    cin >> test;
+    while (test--)
+    {
+        vi v(3);
+        cin >> v[0] >> v[1] >> v[2];
+        sort(v.begin(), v.end());
+        cout << v[1] << endl;
+    }
+
+    return 0;
+}
